f12818f8704a0fdf2db7eb803c35ba408b84832a
chenzp <chenzp@crowddigital.cn>
2023-01-29 16:05:45 +0800

    feat: 初始化工程

---
+++ packages/customize/index.js
@@ -0,0 +1,9 @@
+import ifNewline from './rules/if-newline'
+import importDedupe from './rules/import-dedupe'
+
+export default {
+  rules: {
+    'if-newline': ifNewline,
+    'import-dedupe': importDedupe
+  },
+}
